d56806e87be0bfd0efb8d231e72d7f12d807ba02
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-01-27 12:19:11 -1000

    Initial Commit adding react app + dependencies

---
+++ plant-hydration/src/App.js
@@ -0,0 +1,15 @@
+
+import "bootstrap/dist/css/bootstrap.css";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Plant Hydration
+      </header>
+    </div>
+  );
+}
+
+export default App;
