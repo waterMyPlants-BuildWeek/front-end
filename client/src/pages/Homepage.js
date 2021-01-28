541bb4da516894dc7083d373e71cb15b08698277
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-01-28 11:31:50 -1000

    installed bootstrap dependency

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,26 +1,31 @@
-import { Button } from '@material-ui/core'
-import React from 'react'
-import {useHistory} from 'react-router-dom'
-import styled from 'styled-components'
+import { Button } from "@material-ui/core";
+import React from "react";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Homepage = () => {
+  const history = useHistory();
 
-    const history = useHistory()
+  return (
+    <Wrapper>
+      <div className='title d-flex flex-column justify-content-center align-items-center'> 
+        <h1>Plant Hydration</h1>
+        <h3>Your Personal Plant Watering Reminder Tool</h3>
+      </div>
+      <Button variant="contained" onClick={() => history.push("/dashboard")}>
+        Go to Dashboard
+      </Button>
+    </Wrapper>
+  );
+};
 
-    return (
-        <Wrapper>
-            <h1>I'm the homepage</h1>
-            <Button variant='contained' onClick={() => history.push('/dashboard')}>Go to Dashboard</Button>
-        </Wrapper>
-    )
-}
-
-export default Homepage
+export default Homepage;
 
 const Wrapper = styled.div`
-    display: grid;
-    grid-template-columns: 1fr;
-    padding: 2rem;
-    gap: 1rem;
-    justify-items: center;
-`
+  display: grid;
+  grid-template-columns: 1fr;
+  padding: 2rem;
+  gap: 1rem;
+  justify-items: center;
+`;
