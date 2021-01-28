45544425307f80e91fa1ad86b5e6364f4ce01765
Pete VS <pete_vs88@me.com>
2021-01-28 12:39:12 -0500

    plant modal working

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,12 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import AddPlantForm from '../components/AddPlantForm'
 import MyPlants from '../components/MyPlants'
 import { AuthContext } from '../contexts/Auth'
 import styled from 'styled-components'
+import { Button, Dialog } from '@material-ui/core'
 
 const Dashboard = () => {
 
     const { currentUser } = useContext(AuthContext)
+    const [open, setOpen] = useState(false)
+
+    const handleClickOpen = () => {
+        setOpen(true)
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
 
     return (
         <Wrapper>
@@ -14,8 +24,17 @@ const Dashboard = () => {
                 <h2>Your Details</h2>
                 <h4>{currentUser.email}</h4>
             </div>
+            <MyButton 
+                variant='contained'
+                color='primary'
+                onClick={handleClickOpen}
+            >
+                Add a Plant
+            </MyButton>
+            <Dialog open={open} onClose={handleClose} aria-labelledby='Add a Plant'>
+                <AddPlantForm setOpen={setOpen}/>
+            </Dialog>
             <MyPlants uid={currentUser.uid} />
-            <AddPlantForm />
         </Wrapper>
     )
 }
@@ -27,4 +46,8 @@ const Wrapper = styled.div`
     grid-template-columns: 1fr;
     padding: 2rem;
     gap: 2rem;
+`
+
+const MyButton = styled(Button)`
+    justify-self: start;
 `
