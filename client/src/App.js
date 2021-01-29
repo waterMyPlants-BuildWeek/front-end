05a01f1fb2918a7c08fe803c02a3e45ffb6c0e64
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-01-28 14:04:43 -1000

    completed homepage ready for merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import Login from './pages/Login';
-import Nav from './components/Nav';
-import PrivateRoute from './components/PrivateRoute';
-import Dashboard from './pages/Dashboard'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { AuthProvider } from './contexts/Auth'
-import Homepage from './pages/Homepage'
-import './App.css'
+import React from "react";
+import Login from "./pages/Login";
+import Nav from "./components/Nav";
+import PrivateRoute from "./components/PrivateRoute";
+import Dashboard from "./pages/Dashboard";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/Auth";
+import Homepage from "./pages/Homepage";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 function App() {
-
   return (
-      <AuthProvider>
-        <Router>
-          <div className='App'>
-          <Nav />
-          <Route exact path='/' component={Homepage} />
-          <PrivateRoute path='/dashboard' component={Dashboard}/>
-          <Route path='/login' component={Login} />
-          </div>
-        </Router>
-      </AuthProvider>
+    <AuthProvider>
+      <Router>
+        <Nav />
+        <div className="App container">
+          <Route exact path="/" component={Homepage} />
+          <PrivateRoute path="/dashboard" component={Dashboard} />
+          <Route path="/login" component={Login} />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -2,6 +2,10 @@ import { Button } from "@material-ui/core";
 import React from "react";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
+import pearls from "./images/pearls.jpg";
+import gardening from "./images/gardening.jpg";
+import watering from "./images/watering.jpg";
+import watering2 from "./images/watering2.jpg";
 import "bootstrap/dist/css/bootstrap.css";
 
 const Homepage = () => {
@@ -9,12 +13,84 @@ const Homepage = () => {
 
   return (
     <Wrapper>
-      <div className='title d-flex flex-column justify-content-center align-items-center'> 
-        <h1>Plant Hydration</h1>
+      <div className="title d-flex flex-column justify-content-center align-items-center">
+        <h1 className="display-1">Water My Plants</h1>
         <h3>Your Personal Plant Watering Reminder Tool</h3>
       </div>
-      <Button variant="contained" onClick={() => history.push("/dashboard")}>
-        Go to Dashboard
+      <div className="spacer-vh50"></div>
+      <div className="content-two d-flex flex-column justify-content-around align-items-center">
+        <h3 style={{ marginBottom: "1.5rem" }}>Welcome To Water My Plants</h3>
+        <img
+          src={gardening}
+          className="gardening-sizer"
+          alt="child gardening"
+        />
+        <p>
+          <strong>Water My Plants </strong>
+          is a free app that reminds to you water your plants no matter how big
+          or how small. Whether you frequently forget to water your plants and
+          precious flowers at your home/ office or you would like to set
+          watering cycles for your landscaping management, Water My Plants is
+          the tool that will help to save many lives.{" "}
+        </p>
+      </div>
+      <div className="spacer-vh50"></div>
+      <div className="content-two d-flex flex-row justify-content-around align-items-center">
+        <p>
+          <h3 style={{ marginBottom: "1.5rem" }}>What is Water My Plants</h3>
+          Water My Plants is a free app that reminds to you water your plants no
+          matter how big or how small. Whether you frequently forget to water
+          your plants and precious flowers at your home/ office or you would
+          like to set watering cycles for your landscaping management, Water My
+          Plants is the tool that will help to save many lives.{" "}
+        </p>
+        <img src={pearls} className="pearl-sizer" alt="dragon's pearls" />
+      </div>
+      <div className="spacer-vh50"></div>
+      <div className="content-two d-flex flex-column justify-content-center align-items-center">
+        <h3 style={{ paddingBottom: "3rem" }}>How Do I Use It?</h3>
+        <p style={{ padding: "3rem auto" }}>
+          After setting up your account simply head to your dashboard and begin
+          there! Setting up a new plant to be watered is accomplished in a few
+          easy steps.
+        </p>
+        <br />
+        <ul>
+          <li>Pick a plant to be watered</li>
+          <li>Add a nickname for your plant..</li>
+          <li>Next, enter a species</li>
+          <li>
+            Then, pick a watering cycle appropriate for nourishing your plant -
+            every other day as an example
+          </li>
+          <li>Finally, add a picture of your plant</li>
+          <p>
+            That's it! Simply and easily you have just taken your first steps
+            towards total plant care management and happier healthier plants and
+            flowers.
+          </p>
+        </ul>
+        <img
+          src={watering}
+          className="watering-sizer"
+          alt="watering the garden"
+        />
+        <h4 style={{ padding: "3rem", textAlign: "center" }}>
+          Begin your journey towards ensuring that all of your plants get the
+          nourishment that they deserve. Sign up today and see the difference
+          away. Your plants will thank you.
+        </h4>
+      </div>
+      <div className="spacer-vh50"></div>
+      <div className="content-two d-flex flex-row justify-content-around align-items-center">
+        <img src={watering2} className="happy-sizer" alt="happy gardener" />
+      </div>
+      <Button
+        style={{ margin: "5rem" }}
+        variant="contained"
+        onClick={() => history.push("/dashboard")}
+      >
+        Sign in/ Sign up
       </Button>
     </Wrapper>
   );
